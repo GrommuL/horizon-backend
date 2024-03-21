8e9fda5d0ebc6b7c497bb006b5ae627414c3d4fc
GrommuL <miketsvg@gmail.com>
2024-03-21 12:23:01 +0500

    feat: added resolver methods for updating user profile and searching users in the UserResolver

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { UserResolver } from './user.resolver';
-import { UserService } from './user.service';
+import { Module } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+
+import { UserResolver } from '@/user/user.resolver'
+import { UserService } from '@/user/user.service'
+import { DatabaseModule } from '@/database/database.module'
 
 @Module({
-  providers: [UserResolver, UserService]
+	imports: [DatabaseModule],
+	providers: [UserResolver, UserService, JwtService]
 })
 export class UserModule {}
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,48 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Context, Mutation, Args } from '@nestjs/graphql'
+import { Request } from 'express'
+import { UseGuards } from '@nestjs/common'
+import { createWriteStream } from 'fs'
+import { join } from 'path'
+import { v4 as uuid } from 'uuid'
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js'
+
+import { GraphqlAuthGuard } from '@/auth/graphql-auth.guard'
+import { UserService } from '@/user/user.service'
+import { User } from '@/user/user.type'
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+	constructor(private readonly userService: UserService) {}
+
+	@UseGuards(GraphqlAuthGuard)
+	@Mutation(() => User)
+	async updateProfile(
+		@Args('fullname') fullname: string,
+		@Args('file', { type: () => GraphQLUpload, nullable: true })
+		file: GraphQLUpload.FileUpload,
+		@Context() context: { request: Request }
+	) {
+		const imageUrl = file ? await this.storeImageAndGetUrl(file) : null
+		const userId = context.request.user.sub
+
+		return this.userService.updateProfile(userId, fullname, imageUrl)
+	}
+
+	private async storeImageAndGetUrl(file: GraphQLUpload) {
+		const { createReadStream, filename } = await file
+		const uniqueFilename = `${uuid()}_${filename}`
+		const imagePath = join(process.cwd(), 'public', 'images', uniqueFilename)
+		const imageUrl = `${process.env.APP_URL}/images/${uniqueFilename}`
+		const readStream = createReadStream()
+
+		readStream.pipe(createWriteStream(imagePath))
+
+		return imageUrl
+	}
+
+	@UseGuards(GraphqlAuthGuard)
+	@Query(() => [User])
+	async searchUsers(@Args('fullname') fullname: string, @Context() context: { request: Request }) {
+		return this.userService.searchUsers(fullname, context.request.user.sub)
+	}
+}
